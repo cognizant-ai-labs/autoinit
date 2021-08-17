33edf808ba31488233fa790e5f21be0f5022a2dd
garrettbingham <binghamgarrett@gmail.com>
2021-08-17 18:09:07 +0000

    copyright date 2021

--- autoinit/components/weighted_sum.py
+++ autoinit/components/weighted_sum.py
@@ -1,5 +1,5 @@
 
-# Copyright (C) 2019-2021 Cognizant Digital Business, Evolutionary AI.
+# Copyright (C) 2021 Cognizant Digital Business, Evolutionary AI.
 # All Rights Reserved.
 # Issued under the Academic Public License.
 #
